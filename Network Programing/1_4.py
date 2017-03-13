3249bb1117204076b044b5ea9dd0c8102050f215
suShinGG <chin.lnw@gmail.com>
2017-03-13 05:04:40 +0000

    Add files via upload

---
+++ Network Programing/1_4.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# It may run on any other version with/without modifications.
+import socket
+def find_service_name():
+    protocolname = 'tcp'
+    for port in [80, 25]:
+        print ("Port: %s => service name: %s") %(port, socket.
+        getservbyport(port, protocolname))
+        print ("Port: %s => service name: %s") %(53, socket.
+        getservbyport(53, 'udp'))
+if __name__ == '__main__':
+    find_service_name()
